70a190b99d011b704884f8e3368e749c5b6e24fa
tlw52836 <1660872260@qq.com>
2022-05-11 17:22:09 +0800

    登录接口设计

--- src/main/java/com/tlw/service/impl/LoginServcieImpl.java
+++ src/main/java/com/tlw/service/impl/LoginServcieImpl.java
@@ -9,12 +9,10 @@ import com.tlw.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 
 
 @Service
